6d5bd3851bca2cee165209ee4a871c621a038598
Julia L. Wang <julialong.wang@mail.utoronto.ca>
2021-09-03 03:16:11 -0400

    Logo and projects updates
    - Changed logo design
    - Updated projects page

--- src/components/projects/ProjectsData.jsx
+++ src/components/projects/ProjectsData.jsx
@@ -15,27 +15,29 @@ export const listProjects = [
     button2Link: "https://github.com/JuliaLWang8/DotsLogistics",
   },
   {
-    id: 4,
-    title: "Timbit Dispenser",
-    subtitle: "UofT Eng Kompetition 2020",
+    id: 2,
+    title: "TIMBIT Dispenser",
+    subtitle: "UofT Eng Kompetition (UTEK) 2020",
     description:
       "Arduino (C++) robot designed within 8hours to follow a marked route via sensors and deliver marbles to target houses upon detection.",
     img: "https://github.com/JuliaLWang8/julialwang8.github.io/blob/master/src/media/MeWithRobot.jpg?raw=true",
-    class: "vertical",
+     class: "vertical",
+     button1Type: "fab fa-github",
+     button1Link: "https://github.com/JuliaLWang8/Timbit-Dispenser-Robot",
   },
   {
-    id: 2,
+    id: 3,
     title: "2nd Place Robot",
     subtitle: "Robotics for SpaceEx - SEEK 2019 Competition",
     description:
       "Bluetooth-controlled Arduino (C++) robot designed and built within 6h to complete an obstacle course with an autonomous challenge.",
-    img: "https://github.com/JuliaLWang8/julialwang8.github.io/blob/master/src/media/SEEKBot.jpg?raw=true",
+    img: "https://github.com/JuliaLWang8/julialwang8.github.io/blob/master/src/media/SEEKBot.png?raw=true",
     button1Type: "fas fa-video",
     button1Link:
       "https://drive.google.com/file/d/1arArSZtApMOUZ98M3RPNkBzbZBE-N-c8/view?usp=sharing",
   },
   {
-    id: 3,
+    id: 4,
     title: "SteadyMate",
     subtitle: "UofT Biomedical Engineering Competition 2019",
     description:
@@ -45,7 +47,7 @@ export const listProjects = [
     button1Link: "https://github.com/ntnox/steadymate",
   },
   {
-    id: 6,
+    id: 5,
     title: "BATA Shoe Museum",
     description:
       "Iterative design relative to stakeholders, objectives, and client-given metrics to compile a design brief summarizing shoe storage solutions.",
@@ -58,22 +60,24 @@ export const listProjects = [
       "https://drive.google.com/drive/folders/1OUF_zQq6GR_U6VQ_y9f2YZPLVO1Jy56a?usp=sharing",
   },
   {
-    id: 5,
+    id: 6,
     title: "Clarity",
     subtitle: "Praxis Design Challenge 2020",
     description:
       "Arduino robot designed to travel a certain distance and landing a projectile at a specified target.",
     img: "https://github.com/JuliaLWang8/julialwang8.github.io/blob/master/src/media/ClarityFinal.jpg?raw=true",
+    button1Type: "fas fa-file-alt",
+    button1Link: "https://drive.google.com/file/d/1qRTZm6vK-2Qr4DaVgmjPikN4IiorYhOx/view?usp=sharing",
   },
   {
-    id: 9,
+    id: 7,
     title: "Projectile Slingshot",
     subtitle: "Praxis Design Challenge 2019",
     description: "",
     img: "https://github.com/JuliaLWang8/julialwang8.github.io/blob/master/src/media/Pikachu.jpg?raw=true",
   },
   {
-    id: 7,
+    id: 8,
     title: "Matboard Bridge",
     description:
       "Designed and prototyped a matboard bridge that underwent the Baldwin ",
@@ -82,10 +86,10 @@ export const listProjects = [
       "https://drive.google.com/file/d/14LQaAkfOKYk9O-XczhYrO21m3VzZXm9T/view?usp=sharing",
     button1Type: "fas fa-file-alt",
   },
-  {
-    id: 8,
-    title: "Bridge Redesign",
-    description: "",
-    img: "https://github.com/JuliaLWang8/julialwang8.github.io/blob/master/src/media/BridgeRedesign.jpg?raw=true",
-  },
+  // {
+  //   id: 8,
+  //   title: "Bridge Redesign",
+  //   description: "",
+  //   img: "https://github.com/JuliaLWang8/julialwang8.github.io/blob/master/src/media/BridgeRedesign.jpg?raw=true",
+  // },
 ];
--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,6 +1,5 @@
 import "./projects.scss";
 import { listProjects } from "./ProjectsData.jsx";
-const marble = require("../../media/MeWithRobot.jpg");
 
 export default function Projects() {
   return (
@@ -14,7 +13,7 @@ export default function Projects() {
         extra-curriculars, and competitions. Hover to find out more!
       </p>
       <div className="card-wrapper">
-        {listProjects.map((d) => (
+      {listProjects.map((d) => (
           <div className="proj">
             <img src={d.img} alt="" className={d.class} />
             <div className="overlay">
@@ -32,490 +31,48 @@ export default function Projects() {
             </div>
           </div>
         ))}
+        {/* FLIP STYLE PROJECTS
+        {listProjects.map((d) => (
+          <div className="flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <img src={d.img} alt="" className={d.class} />
+              </div>
+              <div className="flip-card-back">
+                <h3>{d.title}</h3>
+                <h4>{d.subtitle}</h4>
+                <p>{d.description}</p>
+                <div className="btn-container">
+                  <a href={d.button1Link} target="_blank" rel="noreferrer">
+                    <i class={d.button1Type}></i>
+                  </a>
+                  <a href={d.button2Link} target="_blank" rel="noreferrer">
+                    <i class={d.button2Type}></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))} */}
       </div>
+      {/* HEXAGON STYLE PROJECTS
       <div className="hexagon-container">
         <ul id="categories" class="clr">
+        {listProjects.map((d) => (
+          <li>
+          <div>
+            <img
+              src={d.img}
+              alt=""
+            />
+            <h1>{d.title}</h1>
+            <p>{d.description}</p>
+          </div>
+        </li>
+        ))}
           <li class="pusher"></li>
-          <li>
-            <div>
-              <img
-                src="https://farm3.staticflickr.com/2878/10944255073_973d2cd25c.jpg"
-                alt=""
-              />
-              <h1>This is a title a bit longer</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li>
-            <div>
-              <img
-                src="https://farm9.staticflickr.com/8461/8048823381_0fbc2d8efb.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li>
-            <div>
-              <img
-                src="https://farm5.staticflickr.com/4144/5053682635_b348b24698.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li>
-            <div>
-              <img
-                src="https://farm3.staticflickr.com/2827/10384422264_d9c7299146.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li class="pusher"></li>
-          <li>
-            <div>
-              <img
-                src="https://farm7.staticflickr.com/6217/6216951796_e50778255c.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-
-          <li>
-            <div>
-              <img
-                src="https://farm7.staticflickr.com/6083/6055581292_d94c2d90e3.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li class="pusher"></li>
-          <li>
-            <div>
-              <img
-                src="https://farm7.staticflickr.com/6092/6227418584_d5883b0948.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li>
-            <div>
-              <img
-                src="https://farm8.staticflickr.com/7187/6895047173_d4b1a0d798.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li>
-            <div>
-              <img
-                src="https://farm4.staticflickr.com/3766/12953056854_b8cdf14f21.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li>
-            <div>
-              <img
-                src="https://farm7.staticflickr.com/6139/5986939269_10721b8017.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li>
-            <div>
-              <img
-                src="https://farm4.staticflickr.com/3165/5733278274_2626612c70.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li class="pusher"></li>
-          <li class="pusher"></li>
-          <li>
-            <div>
-              <img
-                src="https://farm8.staticflickr.com/7163/6822904141_50277565c3.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li class="pusher"></li>
-          <li>
-            <div>
-              <img
-                src="https://farm4.staticflickr.com/3771/13199704015_72aa535bd7.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li>
-            <div>
-              <img
-                src="https://farm7.staticflickr.com/6083/6055581292_d94c2d90e3.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li>
-            <div>
-              <img
-                src="https://farm8.staticflickr.com/7187/6895047173_d4b1a0d798.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li class="pusher"></li>
-          <li class="pusher"></li>
-          <li>
-            <div>
-              <img
-                src="https://farm3.staticflickr.com/2878/10944255073_973d2cd25c.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li>
-            <div>
-              <img
-                src="https://farm9.staticflickr.com/8461/8048823381_0fbc2d8efb.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li class="pusher"></li>
-          <li>
-            <div>
-              <img
-                src="https://farm5.staticflickr.com/4144/5053682635_b348b24698.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li>
-            <div>
-              <img
-                src="https://farm3.staticflickr.com/2827/10384422264_d9c7299146.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li class="pusher"></li>
-          <li>
-            <div>
-              <img
-                src="https://farm7.staticflickr.com/6217/6216951796_e50778255c.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li>
-            <div>
-              <img
-                src="https://farm7.staticflickr.com/6083/6055581292_d94c2d90e3.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li class="pusher"></li>
-          <li class="pusher"></li>
-          <li>
-            <div>
-              <img
-                src="https://farm7.staticflickr.com/6092/6227418584_d5883b0948.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li>
-            <div>
-              <img
-                src="https://farm8.staticflickr.com/7187/6895047173_d4b1a0d798.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li class="pusher"></li>
-          <li>
-            <div>
-              <img
-                src="https://farm4.staticflickr.com/3766/12953056854_b8cdf14f21.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li>
-            <div>
-              <img
-                src="https://farm3.staticflickr.com/2878/10944255073_973d2cd25c.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li>
-            <div>
-              <img
-                src="https://farm9.staticflickr.com/8461/8048823381_0fbc2d8efb.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li class="pusher"></li>
-          <li>
-            <div>
-              <img
-                src="https://farm5.staticflickr.com/4144/5053682635_b348b24698.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li>
-            <div>
-              <img
-                src="https://farm3.staticflickr.com/2827/10384422264_d9c7299146.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li class="pusher"></li>
-          <li class="pusher"></li>
-          <li>
-            <div>
-              <img
-                src="https://farm7.staticflickr.com/6217/6216951796_e50778255c.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li>
-            <div>
-              <img
-                src="https://farm7.staticflickr.com/6083/6055581292_d94c2d90e3.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li class="pusher"></li>
-          <li class="pusher"></li>
-          <li>
-            <div>
-              <img
-                src="https://farm7.staticflickr.com/6092/6227418584_d5883b0948.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li>
-            <div>
-              <img
-                src="https://farm8.staticflickr.com/7187/6895047173_d4b1a0d798.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li class="pusher"></li>
-          <li>
-            <div>
-              <img
-                src="https://farm4.staticflickr.com/3766/12953056854_b8cdf14f21.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li>
-            <div>
-              <img
-                src="https://farm7.staticflickr.com/6139/5986939269_10721b8017.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li>
-            <div>
-              <img
-                src="https://farm4.staticflickr.com/3165/5733278274_2626612c70.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li class="pusher"></li>
-          <li class="pusher"></li>
-          <li>
-            <div>
-              <img
-                src="https://farm8.staticflickr.com/7163/6822904141_50277565c3.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li class="pusher"></li>
-          <li>
-            <div>
-              <img
-                src="https://farm4.staticflickr.com/3771/13199704015_72aa535bd7.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li>
-            <div>
-              <img
-                src="https://farm7.staticflickr.com/6083/6055581292_d94c2d90e3.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li class="pusher"></li>
-          <li>
-            <div>
-              <img
-                src="https://farm8.staticflickr.com/7187/6895047173_d4b1a0d798.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li>
-            <div>
-              <img
-                src="https://farm7.staticflickr.com/6139/5986939269_10721b8017.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li class="pusher"></li>
-          <li>
-            <div>
-              <img
-                src="https://farm4.staticflickr.com/3165/5733278274_2626612c70.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li class="pusher"></li>
-          <li class="pusher"></li>
-          <li>
-            <div>
-              <img
-                src="https://farm8.staticflickr.com/7163/6822904141_50277565c3.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li class="pusher"></li>
-          <li>
-            <div>
-              <img
-                src="https://farm4.staticflickr.com/3771/13199704015_72aa535bd7.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li>
-            <div>
-              <img
-                src="https://farm7.staticflickr.com/6083/6055581292_d94c2d90e3.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
-          <li class="pusher"></li>
-          <li>
-            <div>
-              <img
-                src="https://farm8.staticflickr.com/7187/6895047173_d4b1a0d798.jpg"
-                alt=""
-              />
-              <h1>This is a title</h1>
-              <p>Some sample text about the article this hexagon leads to</p>
-            </div>
-          </li>
         </ul>
-      </div>
+      </div> */}
     </div>
   );
 }
