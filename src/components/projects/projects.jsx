ed8649557ca1e3e8270fe2f4083007b6dc0d3dd7
Julia L. Wang <wang.julia08@gmail.com>
2023-11-10 03:02:53 -0500

    Project cards v4 initial commit

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,5 +1,8 @@
 import "./projects.scss";
 import { listProjects } from "./ProjectsData.jsx";
+import React from 'react';
+// import styled from 'styled-components';
+// import { Icon } from '@components/icons';
 
 export default function Projects() {
   return (
@@ -11,7 +14,7 @@ export default function Projects() {
       <p>
         Various projects and hackathon submissions I have contributed to, hover for more information.
       </p>
-      <div className="card-wrapper">
+      {/* <div className="card-wrapper">
       {listProjects.map((d) => (
           <div className="proj">
             <img src={d.img} alt="" className={d.class} />
@@ -30,48 +33,35 @@ export default function Projects() {
             </div>
           </div>
         ))}
-        {/* FLIP STYLE PROJECTS
-        {listProjects.map((d) => (
-          <div className="flip-card">
-            <div className="flip-card-inner">
-              <div className="flip-card-front">
+      </div> */}
+      
+      <div className="card-wrapper">
+      {listProjects.map((d) => (
+          <div className="proj">
+            <div className="left">
                 <img src={d.img} alt="" className={d.class} />
-              </div>
-              <div className="flip-card-back">
-                <h3>{d.title}</h3>
-                <h4>{d.subtitle}</h4>
-                <p>{d.description}</p>
-                <div className="btn-container">
+            </div>
+            <div className="right">
+                <p className="project-title arrow">
                   <a href={d.button1Link} target="_blank" rel="noreferrer">
-                    <i class={d.button1Type}></i>
+                    {d.title}
                   </a>
-                  <a href={d.button2Link} target="_blank" rel="noreferrer">
-                    <i class={d.button2Type}></i>
-                  </a>
-                </div>
-              </div>
+                </p>
+                <div className="project-description">{d.description}</div>
+
+                {d.tech && (
+                  <ul className="project-tech-list">
+                    {d.tech.map((tech, i) => (
+                      <li key={i}>{tech}</li>
+                    ))}
+                  </ul>
+                )}
             </div>
           </div>
-        ))} */}
-      </div>
-      {/* HEXAGON STYLE PROJECTS
-      <div className="hexagon-container">
-        <ul id="categories" class="clr">
-        {listProjects.map((d) => (
-          <li>
-          <div>
-            <img
-              src={d.img}
-              alt=""
-            />
-            <h1>{d.title}</h1>
-            <p>{d.description}</p>
-          </div>
-        </li>
         ))}
-          <li class="pusher"></li>
-        </ul>
-      </div> */}
+      </div>
     </div>
   );
 }
+
+      
