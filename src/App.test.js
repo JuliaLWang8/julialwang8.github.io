7ace35f1dc1281b722585b98951e3057b7a6d38c
Julia L. Wang <wang.julia08@gmail.com>
2023-12-24 00:47:42 -0500

    fixed tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,31 +1,33 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
-import Navbar from "./components/navbar/navbar";
 import Home from "./components/home/home";
 import About from "./components/about/about";
 import Resume from "./components/resume/resume";
 import Projects from "./components/projects/projects";
-import Contact from "./components/contact/contact";
+import { mockAllIsIntersecting } from 'react-intersection-observer/test-utils';   
 
 test('home', () => {
   render(<Home />);
+  mockAllIsIntersecting(true)
   expect(screen.getByText(/Julia L. Wang/)).toBeInTheDocument();
 });
 
 test('about', () => {
   render(<About />);
+  mockAllIsIntersecting(true)
   expect(screen.getByText(/Engineering Science/)).toBeInTheDocument();
   expect(screen.getByText(/I am a/)).toBeInTheDocument();
 });
 
 test('projs', () => {
   render(<Projects />);
+  mockAllIsIntersecting(true)
   expect(screen.getByText(/Covalent Reactivity/)).toBeInTheDocument();
   expect(screen.getByText(/Show More/)).toBeInTheDocument();
 });
 
 test('resume', () => {
   render(<Resume />);
+  mockAllIsIntersecting(true)
   expect(screen.getByText(/Intel/)).toBeInTheDocument();
   expect(screen.getByText(/MAY 2022/)).toBeInTheDocument();
 
